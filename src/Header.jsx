d28e7a45ee55cf8582177947ac9bd40a69c6160b
Ephriam Kassa <ephriam123@gmail.com>
2024-06-22 16:02:52 -0700

    ere

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className="header">
       <img src='./foxone.jpeg' alt="Michi" className="logo" />
-      <span className="header-name">$SPANDA</span>
+      <span className="header-name">$FOX</span>
     </header>
   );
 };
